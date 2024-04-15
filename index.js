552badbcf859b0362d04b0c6460b5f0e11f47779
Mattias007 <mattiasmaasik@ametikool.ee>
2024-04-15 11:05:27 +0300

    topics for automatic controll

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 // MQTT broker URL
-const mqttBrokerUrl = 'mqtt://127.0.0.1';
+const mqttBrokerUrl = 'mqtt://85.89.43.95';
 
 const mqttClient = mqtt.connect(mqttBrokerUrl,{
     username: "mattias",
@@ -15,18 +15,38 @@ const mqttClient = mqtt.connect(mqttBrokerUrl,{
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
     mqttClient.subscribe("AC/+/web");
+    mqttClient.subscribe("AC/+/Override");
 
     
 
 });
 mqttClient.on('message',(topic, message) => {
+    const topicarray = topic.split("/")
     message = JSON.parse(message.toString())
-    const name = message.name
-    delete(message.id)
-    delete(message.name)
-    console.log(`http://85.89.32.58/input/post?node=${name}&fulljson=${JSON.stringify(message)}&apikey=17bda09eb30a8f93c375d009a6066c2c`)
-   fetch(`http://85.89.32.58/input/post?node=${name}&fulljson=${JSON.stringify(message)}&apikey=17bda09eb30a8f93c375d009a6066c2c`).then(r => {
-   })
+    if (topicarray[2] == "web"){
+        const name = message.name
+        delete(message.id)
+        delete(message.name)
+        fetch(`http://85.89.32.58/input/post?node=${name}&fulljson=${JSON.stringify(message)}&apikey=17bda09eb30a8f93c375d009a6066c2c`).then(r => {
+        })
+    }
+    else if (topicarray[2] == "Override"){
+        switch (message){
+            case 0:
+            mqttClient.publish(`AC/${topicarray[1]}/Command`,0, { qos: 0, retain: true }, (error) => {
+                if (error) {
+                    console.error(error)
+                }
+            })
+            break
+            case 1:
+                console.log(message)
+                // logic for solar
+            case 2:
+                console.log(message)
+                // logic for power meeter
+        }
+    }
 
 });
 
